27f4abf29a3e8883171623c378174b0271684de2
Alline <alline_franciely@hotmail.com>
2022-02-02 18:04:56 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const shopCartItems = document.querySelector('.cart_items');
+const sectionItems = document.querySelector('.items');
+
 const cartItems = document.getElementsByClassName('cart_items');
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -41,9 +44,17 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-async function searchProducts(product) {
+// Cria a função de carregando: 
+const loading = () => {
+  const loadingAPI = document.createElement('h3');
+  loadingAPI.className = 'loading';
+  loadingAPI.innerText = 'carregando...';
+  sectionItems.appendChild(loadingAPI);
+};
+// Adiciona lista de produtos: 
+  const searchProducts = async (product) => {
+    loading();
   const search = await fetchProducts(product);
-  const sectionItems = document.querySelector('.items');
   console.log(search);
   search.results.forEach((item) => {
     const productProject = {
@@ -54,8 +65,20 @@ async function searchProducts(product) {
     const productItem = createProductItemElement(productProject);
     sectionItems.appendChild(productItem);
   });
-}
+};
+const addProduct = async (item) => {
+  const addData = await fetchItem(item);
+  const addItem = {
+    sku: addData.id,
+    name: addData.title,
+    salePrice: addData.price,
+  };
+  const itemSelected = createCartItemElement(addItem);
+  shopCartItems.appendChild(itemSelected);
+  saveCartItems(shopCartItems.innerHTML);
+  addPrice(itemObject.saleProce);
+};
 
 window.onload = () => { 
   searchProducts('computador');
-};
+}; 
