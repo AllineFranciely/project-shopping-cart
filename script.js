3bacba3b5daf4f94e690ba73a6a2042679aab233
Alline <alline_franciely@hotmail.com>
2022-02-04 13:50:35 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// const sectionItems = document.querySelector('.items');
-// const cartItems = document.getElementsByClassName('cart__items');
-const emptyCart = document.querySelector('.empty-cart');
-const cart = document.querySelector('.cart');
+const sectionItems = document.querySelector('.cart__items');
 const itemsScreen = document.querySelector('.items');
+const totalPrice = document.querySelector('.total-price');
 
+// Cria elemento de imagem e adiciona na tela no fornecimento do parâmetro:
 function createProductImageElement(imageSource) {
 const img = document.createElement('img');
 img.className = 'item__image';
 img.src = imageSource;
 return img;
 }
+
+// Cria novos elementos ao receber os parâmetros:
 function createCustomElement(element, className, innerText) {
 const e = document.createElement(element);
 e.className = className;
@@ -17,12 +18,12 @@ e.innerText = innerText;
 return e;
 }
 
+// Elimina item clicado do cart (requisito 3): 
 function cartItemClickListener(event) {
-cartItems = document.querySelector('.cart__items');
-cartItems.removeChild(event.target);
-saveCartItems(cartItems.innerHTML);
+event.target.remove();
 }
 
+// Cria o item da lista:
 function createCartItemElement({ sku, name, salePrice }) {
 const li = document.createElement('li');
 li.className = 'cart__item';
@@ -31,18 +32,21 @@ li.addEventListener('click', cartItemClickListener);
 return li;
 }
 
+// Puxa o parent do botão e encontra o ID na classe item__sku (eequisito 2):
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// Requisito 2:
 function clickCart(product) {
-  const sectionItems = document.querySelector('.cart__items');
-  const itemsCart = createCartItemElement(product);
-  sectionItems.appendChild(itemsCart);
+  // const sectionItems = document.querySelector('.cart__items');
+const itemsCart = createCartItemElement(product);
+sectionItems.appendChild(itemsCart);
 }
 
+// Requisito 2: 
 const addProduct = async (event) => {
-  const items = getSkuFromProductItem(event.target.parentElement); // estudar parentElement
+const items = getSkuFromProductItem(event.target.parentElement); // estudar parentElement
 const addData = await fetchItem(items); // traz objeto com atributos do produto.
 const infos = {
 sku: addData.id,
@@ -52,34 +56,19 @@ salePrice: addData.price,
 clickCart(infos);
 };
 
-/* sectionItems.addEventListener('click', async (event) => {
-const itemID = event.target.parentNode.firstChild.innerText;
-const addItem = await addProduct(id);
-cartItems.appendChild(addItem);
-saveCartItems(cartItems.innerHTML);
-}); 
-
-function productsButtonCart() {
-const buttons = document.querySelectorAll('.item_add');
-buttons.forEach((button) => {
-button.addEventListener('click', addProduct);
-});
-} */
-
+// Ao receber o obj cria e organiza os elementos através dos dados obtidos:
 function createProductItemElement({ sku, name, image }) {
 const section = document.createElement('section');
 section.className = 'item';
 section.appendChild(createCustomElement('span', 'item__sku', sku));
 section.appendChild(createCustomElement('span', 'item__title', name));
 section.appendChild(createProductImageElement(image));
-
 const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
 section.appendChild(button);
-// button.addEventListener('click', () => addProduct(id));
 return section;
 }
 
-// Cria a função de carregando: 
+// Cria a função de carregando (requisito 7): 
 const loading = () => {
 const loadingAPI = document.createElement('h3');
 loadingAPI.className = 'loading';
@@ -87,10 +76,10 @@ loadingAPI.innerText = 'carregando...';
 itemsScreen.appendChild(loadingAPI);
 };
 
-// Adiciona lista de produtos: 
-const searchProducts = async (product) => {
+// Adiciona lista de produtos (requisito 1): 
+const searchProducts = async (products) => {
 loading();
-const search = await fetchProducts(product);
+const search = await fetchProducts(products);
 // console.log(search);
 search.results.forEach((item) => {
 const productProject = {
@@ -106,21 +95,17 @@ const load = document.querySelector('.loading');
 load.remove();
 };
 
-function clear() {
-emptyCart.addEventListener('click', () => {
-cartItems.innerHTML = '';
-localStorage.clear();
-document.querySelector('.price').innerText = 0;
+const buttonCleadCart = document.querySelector('.empty-cart');
+buttonCleadCart.addEventListener('click', () => {
+sectionItems.innerHTML = '';
 });
-}
 
-/* function restoreItems() {
-  sectionItems.innerHTML = getSavedCartItems();
-const cartItem = document.querySelectorAll('.cart__item');
-cartItem.forEach((item) => item.addEventListener('click', cartItemCLickListener));
-} */ 
+function restoreItems() {
+const loadItems = getSavedCartItems();
+sectionItems.innerHTML = loadItems;
+} 
 
 window.onload = () => { 
 searchProducts('computador');
-// restoreItems();
+restoreItems();
 }; 
